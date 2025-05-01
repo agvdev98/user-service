1a0f444d5b3b0f644052aeb3f6e4f7da50baa0ee
Andres Gomez <agomez@allcode.com>
2025-05-01 17:26:31 -0500

    Add repository and its implementation

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,1 +1,11 @@
 package repository
+
+import "github.com/agvdev98/user-service/internal/model"
+
+type UserRepository interface {
+	CreateUser(user *model.User) (*model.User, error)
+	FindUserByID(id uint) (*model.User, error)
+	FindAllUsers() ([]model.User, error)
+	UpdateUser(user *model.User) (*model.User, error)
+	DeleteUser(id uint) error
+}
---
+++ internal/repository/user_repository_impl.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"github.com/agvdev98/user-service/internal/model"
+	"gorm.io/gorm"
+)
+
+type userRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepositoryImpl{db: db}
+}
+
+func (r *userRepositoryImpl) CreateUser(user *model.User) (*model.User, error) {
+	if err := r.db.Create(user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (r *userRepositoryImpl) FindUserByID(id uint) (*model.User, error) {
+	var user model.User
+	if err := r.db.First(&user, id).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepositoryImpl) FindAllUsers() ([]model.User, error) {
+	var users []model.User
+	if err := r.db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (r *userRepositoryImpl) UpdateUser(user *model.User) (*model.User, error) {
+	if err := r.db.Save(user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (r *userRepositoryImpl) DeleteUser(id uint) error {
+	return r.db.Delete(&model.User{}, id).Error
+}
