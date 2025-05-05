4829423515369322429ed448633b98c782b52fca
Andres Gomez <agomez@allcode.com>
2025-05-05 15:05:47 -0500

    Add cors config

---
+++ internal/security/cors.go
@@ -0,0 +1,16 @@
+package security
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func SetupCORS(r *gin.Engine) {
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:4200"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
+	config.AllowCredentials = true
+	config.MaxAge = 12 * time.Hour
+}
